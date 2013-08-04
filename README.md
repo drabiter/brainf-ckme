96b8c61eaa5896e2ca735039c17ee78554460aed
Hendra Gunawan <drabiter@gmail.com>
2013-08-04 11:44:53 +0700

    Clean gh-pages

--- README.md
+++
@@ -1,4 +0,0 @@
-brainf-ckme
-===========
-
-Write the f*cking Brainf*ck code and run the f*ck on the fly.
--- js/app.js
+++ js/app.js
@@ -1,36 +1,7 @@
 (function(){
   'use strict';
 
-  var A = [0];
-  var pointer = 0;
-  // because length call is expensive
-  var length = 1;
-
-  var code = [];
-  var jump = [];
-  var cursor = 0;
-
-  var input = [];
-  var output = [];
-
-  var inputPtr = 0;
-  var inputLength = 0;
-
-  var resetBF = function() {
-    A.length = 0;
-    A.push(0);
-    pointer = 0;
-    length = 1;
-
-    code.length = 0;
-    jump.length = 0;
-    cursor = 0;
-
-    input.length = 0;
-    output.length = 0;
-    inputPtr = 0;
-    inputLength = 0;
-  };
+  var Interpreter = new Brainfuckme();
 
   var $ = function(selector) {
     return document.querySelector(selector);
@@ -51,90 +22,11 @@
   var outputEl = $$('#output', mainEl);
   var consoleEl = $$('#console', mainEl);
 
-  var interpreter = function(token) {
-    switch (token) {
-      case '+':
-        ++A[pointer];
-        break;
-      case '-':
-        --A[pointer];
-        break;
-      case '>':
-        pointer++;
-        if (pointer == length) {
-          length++;
-          A.push(0);
-        }
-        break;
-      case '<':
-        if (pointer > 0) {
-          pointer--;
-        }
-        break;
-      case '.':
-        output.push(String.fromCharCode(A[pointer]));
-        break;
-      case ',': 
-        if (inputPtr < input.length) {
-          A[pointer] = input.charCodeAt(inputPtr);
-          inputPtr++;
-        }
-        break;
-      case '[':
-        if (A[pointer] > 0) {
-          jump.push(cursor);
-        } else {
-          var _sum = code.length;
-          var _stack = 0;
-          for (var i = cursor + 1; (i < _sum || cursor == i); i++) {
-            if (code[i] == ']') {
-              if (_stack === 0) {
-                cursor = i;
-              } else {
-                _stack--;
-              }
-            } else if (code[i] == '[') {
-              _stack++;
-            }
-          }
-        }
-        break;
-      case ']':
-        if (A[pointer] <= 0) {
-          jump.pop();
-        } else {
-          cursor = jump[jump.length - 1];
-          console.log('jump to '+cursor);
-        }
-        break;
-      default:
-    }
-    cursor++;
-  };
-   
-  var logToken = function(c, token) {
-    console.log('A[' + pointer + '] = ' + A[pointer] + ' next: ' + token + ' line ' + c);
-    console.log('  - ' + A);
-  };
-
-  var run = function() {
-    code = consoleEl.value.split('');
-    input = inputEl.value;
-    var _sum = code.length;
-    for (cursor = 0; cursor < _sum;) {
-      logToken(cursor, code[cursor]);
-      interpreter(code[cursor]);
-    }
-    logToken(cursor, code[cursor]);
-    outputEl.value = output.join('');
-  };
-
   var inputsBtn = $$$('.btnInput');
   for (var i = 0, sum = inputsBtn.length; i < sum; i++) {
     inputsBtn[i].onclick = function(e) {
       e.preventDefault();
       consoleEl.value += this.text;
-      // consoleEl.focus();
     };
   }
 
@@ -142,28 +34,40 @@
   backBtn.onclick = function(e) {
     e.preventDefault();
     consoleEl.value = consoleEl.value.split('').slice(0, -1).join('');
-    // consoleEl.focus();
   };
 
   var runBtn = $('#btnRun');
   runBtn.onclick = function(e) {
     e.preventDefault();
-    this.disabled = true;
-    resetBF();
+
+    var button = this;
+    
+    button.disabled = true;
+    Interpreter.reset();
     outputEl.value = '';
-    run();
-    this.disabled = false;
+    
+    console.time('run');
+    Interpreter.run(
+      consoleEl.value,          // source code
+      inputEl.value.split(''),  // input data
+      {log: true},              // options
+      function(output){         // callback
+        console.log(output);
+        outputEl.value = output.join('');
+        button.disabled = false;
+        console.timeEnd('run');
+      });
   };
 
   var resetBtn = $('#btnReset').onclick = function(e) {
     e.preventDefault();
-    resetBF();
+    Interpreter.reset();
     consoleEl.value = '';
-    // consoleEl.value = ' ++++++++++[>+++++++<-]>.';
   };
 
 }());
 
+// consoleEl.value = ' ++++++++++[>+++++++<-]>.';
 // ++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
 // ++++++++++[>+++++++<-]>.
 // +++[>++<-]>.
